908becb4526c5264d9bb9291ec5dc64facb7a946
Dennis Russell <djjrussell@gmail.com>
2023-12-06 14:48:25 -0800

    adding hover effect

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -5,7 +5,7 @@ import { BsFillPlusCircleFill } from "react-icons/bs";
 const NavBar = () => {
   return (
     <nav className="bg-indigo-500 px-8 py-3 flex w-full justify-between drop-shadow-lg rounded-3xl align-middle items-center">
-      <Link href="/experiences/auth/">
+      <Link href="/experiences/auth/" className="hover:scale-105">
         <h1 className="text-white font-bolder cursor-pointer text-3xl tracking-[-3px]">
           BishList
         </h1>
